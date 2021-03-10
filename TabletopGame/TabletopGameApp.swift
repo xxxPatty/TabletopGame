af65bb216e2f855560ad840c3a80f3ea5e7564d8
Patty <L224749391@gmail.com>
2021-03-10 14:37:33 +0800

    Initial Commit

---
+++ TabletopGame/TabletopGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabletopGameApp.swift
+//  TabletopGame
+//
+//  Created by 林湘羚 on 2021/3/10.
+//
+
+import SwiftUI
+
+@main
+struct TabletopGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
